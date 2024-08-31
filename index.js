3e6d7eb436506ff2cf2f9e35dec26d1572b65e70
Yoeees <107732257+Yoeees@users.noreply.github.com>
2024-08-31 21:05:07 +0800

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-const openBtn = document.getElementById("inv-button");
-const submitBtn = document.getElementById("submit");
-const modal = document.getElementById("pop-up");
-
-openBtn.addEventListener("click",() => {
-    modal.classList.add("open");
-});
